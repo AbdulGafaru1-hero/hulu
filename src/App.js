d157143cc2e5cd654a45a03fa8e3eec3d226b2a0
AbdulGafaru1-hero <yahayaabdulgafaru1@gmail.com>
2023-03-13 13:37:31 +0000

    api request from tmdb

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 
+import { useState } from 'react';
 import './App.css';
 import Header from './Header';
+import Nav from './Nav';
+import requests from './requests';
+import Results from './Results';
 
 function App() {
+
+  const [selectedOption, setSelectedOption] = useState(requests.fetchTopRated);
+
+
   return (
     <div className="app">
   
-       <Header/>
+      <Header />
+      
+
+      <Nav setSelectedOption={setSelectedOption}/>
+      
+
+       <Results selectedOption={selectedOption}/>
     </div>
  
-    // Nav
-    // 
   
   );
 
